62e33d77319037bd473c726c2c0b03d775d67ff0
Deepesh K N <deepesh@ost.com>
2019-08-24 12:27:40 +0200

    Made web3 compatible

--- AppContainer.js
+++ AppContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './global';
 import RootNavigationContainer from './RootNavigationContainer';
 
 export default class AppContainer extends Component {
